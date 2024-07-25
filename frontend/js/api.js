4031adbc505a52804f65615b7dd2305b3259be3c
Gelvazio <gelvazio@hotmail.com>
2024-07-24 22:03:26 -0300

    todos

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -93,3 +93,45 @@ function callApiPost(method, rota, fn = false, body = false) {
     console.log("Erro:" + error);
   }
 }
+
+
+function callApiCep(method, rota, fn = false) {
+  const url = rota;//getUrlBase() + getVersao() + rota;
+
+  console.log("URL CHAMADA:" + url);
+  try {
+    fetch(url, {
+      method: method, // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, *cors, same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin", // include, *same-origin, omit
+      headers: getHeadersApiCep(),
+      redirect: "follow", // manual, *follow, error
+      referrerPolicy: "no-referrer",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        let oRetorno = JSON.stringify(data);
+
+        console.log(data);
+        // Se existir a funcao, executa a funcao
+        // passando por parametro o retorno da API
+        if (fn) {
+          // executando a funcao
+          fn(data);
+        }
+      });
+  } catch (error) {
+    console.log("Erro:" + error);
+  }
+}
+
+function getHeadersApiCep() {
+  return {
+    "Content-Type" : "application/json",
+    //"Accept": "application/json",
+    //'content-type': 'application/json;charset=utf-8',
+    //"charset":"utf-8",
+    //"Access-Control-Allow-Origin":"*"
+  };
+}
